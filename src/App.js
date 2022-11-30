dba2cd39dd254ad00e74fff8898fed2dfb4b5dce
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-11-30 23:33:46 +0900

    add sphere

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Canvas } from "@react-three/fiber";
+import Sphere from "./components/Sphere";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <Sphere />
+          <ambientLight intensity={1} position={[10,10,10]} angle={0.15} penumbra={1}/>
+          <pointLight position={[-10,-10,-10]}/>
+        </Canvas>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Sphere.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useSpring, animated, config } from "@react-spring/three";
+
+function Sphere() {
+  const ref = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const { scale } = useSpring({
+    scale: hovered ? 1.5 : 1,
+    config: config.wobbly,
+  });
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+
+  return (
+    <>
+      <animated.mesh
+        ref={ref}
+        onClick={() => setClicked(!clicked)}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+        scale={scale}
+      >
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color={clicked ? "blue" : "purple"} />
+      </animated.mesh>
+    </>
+  );
+}
+
+export default Sphere;
