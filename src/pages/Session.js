035af2a017bed06530da4d536eb9a51a2b0cbf88
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-12-02 00:13:53 +0900

    chat page

---
+++ src/pages/Session.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+
+const Session = () => {
+  return (
+    <ChatEngine
+    publicKey={'0e62100a-d070-4c57-90ab-a5a38f05f4b4'}
+    userName={'ley'}
+    userSecret={'1234'}
+  />
+  )
+}
+export default Session;
