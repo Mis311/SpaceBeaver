dab0528c26c042962010222d2bdfc8e58d9435d4
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-12-05 14:36:39 +0900

    spinner

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -1,22 +1,39 @@
 import { useRef } from "react";
+import { smootherstep } from "three/src/math/MathUtils";
 
 const SpinWheel = () => {
-    let wheel = useRef("");
-    let button = useRef("");
-    let number =Math.ceil(Math.random()*1000);
-      return (
-        <>
-        <button ref={button} onClick={
-          () => { wheel.current.style.transform = "rotate(" + number + "deg)";
-          wheel.current.style.transtion = "all 10s ease-out";
-          number += Math.ceil(Math.random()*1000) }
-        }> Spin the wheel</button>
-        <div className="wheelContainer">
-          <img src=".././pics/wheel.png" alt="wheel" className="wheel" ref={wheel}></img> 
-        </div>
-        
-         </>
-  )
-}
+  let wheel = useRef("");
+  let button = useRef("");
+
+  //make it more smooth spinning
+  const spin = () => {
+    let number = Math.ceil(Math.random() * 10000);
+    let random = Math.floor(Math.random() * 360);
+    let total = random + number;
+    wheel.current.style.transform = `rotate(${total}deg)`; //rotate the wheel
+  };
+  return (
+    <>   
+      <button
+        ref={button}
+        onClick={() => {
+          spin();
+        }}
+      >
+        Spin the wheel
+      </button>
+      <div className="wheelContainer">
+        <img
+          src=".././pics/wheel.png"
+          alt="wheel"
+          className="wheel"
+          ref={wheel}
+        ></img>
+      </div>
+    </>
+  );
+};
+
 
 export default SpinWheel;
+
