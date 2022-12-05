d205d5c77d369738c8f8955795ba9b955df61a36
Mis311 <ley.hanamura@gmail.com>
2022-12-05 18:49:12 +0500

    Updating wheel

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -1,72 +1,70 @@
 import { useRef, useState } from "react";
-// import { smootherstep } from "three/src/math/MathUtils";
 
 const SpinWheel = () => {
   let wheel = useRef("");
   let button = useRef("");
-  //**usestate framework*/
-   const [show, setShow] = useState({ 
-    show: false,
-    setShow: false,
-  });
+  let total;
 
-function changeShow(){
-    setShow(!show);
-}
+  const [show, setShow] = useState(false),
+    [item, setItem] = useState("");
 
-if (changeShow){
+  if (show) {
     return (
-        <div>
-            <h1>{Prize(total)}</h1>
-        </div>
-    )
-}
-
+      <div>
+        <h1>{Prize(total)}</h1>
+      </div>
+    );
+  }
 
   const spin = () => {
     let number = Math.ceil(Math.random() * 10000);
     let random = Math.floor(Math.random() * 360);
-    let total = random + number;
+    total = random + number;
+
     wheel.current.style.transform = `rotate(${total}deg)`; //rotate the wheel
     button.current.style.pointerEvents = "none"; //disable the button
     wheel.current.style.transition = "all 5s ease-out"; //add transition
+
     setTimeout(() => {
       button.current.style.pointerEvents = "auto"; //enable the button
       wheel.current.style.transition = "none"; //remove transition
       let deg = total % 360; //find the degree
       wheel.current.style.transform = `rotate(${deg}deg)`; //rotate the wheel
-    }, 10000);
-
-
-
+    }, 5500);
   };
 
   function Prize(deg) {
-    if (deg % 0 <= 0 && deg % 72 >= 0) {
-      alert("You won a new ticket for book!");
-    }  if(deg % 0 <= 72 && deg % 144 >= 0 ) {
-      alert("You won a new ticket for astro item!");
-    } if(deg % 0 <= 144 && deg % 216>=0 ) {
-      alert("You won a new secret item!");
-    } if (deg % 0<= 216 && deg %288>= 0) {
-      alert("You won a new ticket for build material!");
-    }if (deg % 0 <= 288 && deg % 365>=0 ) {
-      alert("You won a new ticket for beaver item!");
-    
-    }
-  };
-  
+    if (deg % 0 <= 0 && deg % 72 >= 0) setItem("book");
+
+    if (deg % 0 <= 72 && deg % 144 >= 0) setItem("ticket for astro");
+
+    if (deg % 0 <= 144 && deg % 216 >= 0) setItem("secret item");
+
+    if (deg % 0 <= 216 && deg % 288 >= 0) setItem("ticket for build material");
+
+    if (deg % 0 <= 288 && deg % 365 >= 0) setItem("ticket for beaver item");
+  }
+
   return (
     <>
-      <button className="spin-button" ref={button} onClick={spin}>
-        Spin the wheel
-      </button>
-      
-      <img src="../pics/wheel.png" alt="wheel" ref={wheel}></img>
-   
+      {/* Wheel */}
+      <div>
+        <button className="spin-button" ref={button} onClick={spin}>
+          Spin the wheel
+        </button>
+
+        <img src="../pics/wheel.png" alt="wheel" ref={wheel}></img>
+      </div>
+
+      {/* Prize */}
+      <div className="wheel__prize">
+        <div className="wheel__prize_text">
+          You won a {Prize(total)} {item}!
+        </div>
+        <div className="wheel__prize_image"></div>
+      </div>
     </>
   );
-  };
-
+};
 
 export default SpinWheel;
