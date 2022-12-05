b0682186b0bee1d45385bb36245598f582907eaf
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-12-05 21:38:04 +0900

    spin  ratio

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -18,15 +18,38 @@ const SpinWheel = () => {
       let deg = total % 360; //find the degree
       wheel.current.style.transform = `rotate(${deg}deg)`; //rotate the wheel
     }, 10000);
+
+    <div className="test" onTransitionEnd={() => Prize()}><h1>test + {Prize()}</h1>
+
+  </div>
   };
+
+  function Prize(deg) {
+    if (deg % 0 <= 0 && deg % 72 >= 0) {
+      alert("You won a new ticket for book!");
+    }  if(deg % 0 <= 72 && deg % 144 >= 0 ) {
+      alert("You won a new ticket for astro item!");
+    } if(deg % 0 <= 144 && deg % 216>=0 ) {
+      alert("You won a new secret item!");
+    } if (deg % 0<= 216 && deg %288>= 0) {
+      alert("You won a new ticket for build material!");
+    }if (deg % 0 <= 288 && deg % 365>=0 ) {
+      alert("You won a new ticket for beaver item!");
+    
+    }
+  };
+  
   return (
     <>
       <button className="spin-button" ref={button} onClick={spin}>
         Spin the wheel
       </button>
+
       <img src="../pics/wheel.png" alt="wheel" ref={wheel}></img>
+   
     </>
   );
-};
+  };
+
 
 export default SpinWheel;
